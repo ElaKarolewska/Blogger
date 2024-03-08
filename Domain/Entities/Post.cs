0fe46a9035b664f79dc98f452bfdbbe876808dca
ElaKarolewska <138811002+ElaKarolewska@users.noreply.github.com>
2024-03-08 21:28:28 +0100

    3.2

---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Post: AuditableEnitity
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public Post() {}
+
+        public Post(int id, string name, string content)
+        {
+            Id = id;
+            Name = name;
+            Content = content;
+        }
+    }
+}
---
+++ Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,45 @@
+﻿
+
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private static readonly ISet<Post> _posts = new HashSet<Post>()
+        {
+            new Post (1,"Treść 1","Treść 1"),
+            new Post (2,"Treść 2","Treść 2"),
+            new Post (3,"Treść 3","Treść 3"),
+        };
+        public IEnumerable<Post> GettAll()
+        {
+            return _posts;
+        }
+
+        public Post GetById(int id)
+        {
+            return _posts.SingleOrDefault(x => x.Id == id);
+        }
+
+        public Post Add(Post post)
+        {
+            post.Id = _posts.Count() + 1;
+            post.Created = DateTime.Now;
+            _posts.Add(post);
+            return post;
+        }
+
+        public void Update(Post post)
+        {
+            post.LastModified = DateTime.Now;
+        }
+
+        public void Delete(Post post)
+        {
+            _posts.Remove(post);
+        }
+
+    }
+}
